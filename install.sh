1a607fcb9cd73f6cd1cc1ccb6dd6de539da0b43e
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-02-10 21:00:41 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install wget
+sudo apt-get install curl
+
+echo 'Installed wget and curl'
+
 
 ## mongo-db installation
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -   
@@ -12,10 +16,18 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongod
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
+echo "Installed mongodb"
+
 ## nodejs installation
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+echo "Installed nodejs"
+
+## install npm
+#curl -L https://npmjs.org/install.sh | sudo sh
+sudo apt install npm
+
+echo "installed npm"
 # Any configuration related commands
 sudo apt-get update
-
